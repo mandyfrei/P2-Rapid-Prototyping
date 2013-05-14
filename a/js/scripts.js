1ed5cdc0fee7cb927bc89b29dde2f3bf54399cc4
Mandy frei <mandy.frei@eagles.ewu.edu>
2013-05-14 15:28:16 -0700

    Updated

---
+++ a/js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  
+  // Good Stuff
+  
+  $('.dropdown-toggle').dropdown()
+  
+  $('.carousel').carousel('cycle');
+  
+	$(".collapse").collapse({
+	parent: false
+	})
+  
+});	
+
+	
---
+++ b/js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  
+  // Good Stuff
+  
+  $('.nav-tabs').button()
+  
+  $('.dropdown-toggle').dropdown()
+
+	$(".collapse").collapse({
+	parent: false
+	})
+  
+  
+});	
